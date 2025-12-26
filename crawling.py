e72296025b3d25a01adcca836f046c24126cccaa
samuel5721 <samuel20070731@gmail.com>
2025-12-26 09:13:16 +0900

    fix: map type change

--- crawling.py
+++ crawling.py
@@ -100,10 +100,7 @@ def get_verce_texts(driver) :
 	return [i for i in verses if i not in verse_nums]
 
 def get_titles(driver) :
-	return driver.find_elements(By.CLASS_NAME, "ms")
-
-def get_subtitles(driver) :
-	return driver.find_elements(By.CLASS_NAME, "s")
+	return driver.find_elements(By.CSS_SELECTOR, ".ms, .s")
 
 def get_paragraphs(driver) :
 	return driver.find_elements(By.CSS_SELECTOR, ".p, .m")
@@ -154,12 +151,10 @@ def main():
 	try:
 		all_verse_maps = []
 		all_title_maps = []
-		all_subtitle_maps = []
 		all_paragraph_maps = []
-		all_quote_maps = []
 		all_footnote_maps = []
 
-		for i in range(2):
+		for i in range(5):
 			testament = "GEN"
 			chapter = i + 1
 			url = f"https://www.bskorea.or.kr/KNT/index.php?chapter={testament}.{chapter}"
@@ -174,9 +169,7 @@ def main():
 			# 이 장에서 사용할 WebElement 들만 가져오기
 			ch_verse_texts = get_verce_texts(driver)
 			ch_titles = get_titles(driver)
-			ch_subtitles = get_subtitles(driver)
 			ch_paragraphs = get_paragraphs(driver)
-			ch_quotes = get_quotes(driver)
 			ch_footnotes = get_footnotes(driver)
 
 			# 절 본문 전처리
@@ -191,16 +184,20 @@ def main():
 							temp_text += '\n'
 					temp_text += verse_text.get_attribute("textContent")
 				else:
-					ch_verse_maps.append({temp_source: temp_text})
+					ch_verse_maps.append({'id': temp_source, 'text': temp_text})
 					temp_source = now_source
 					temp_text = verse_text.get_attribute("textContent")
 
 			# 첫 번째 dummy(None) 제거 및 마지막 절 추가
 			ch_verse_maps.pop(0)
-			ch_verse_maps.append({temp_source: temp_text})
+			ch_verse_maps.append({'id': temp_source, 'text': temp_text})
 
-			# verse_maps는 서명.장.절을 키로 하고 그 절의 내용을 값으로 하는 딕셔너리의 리스트입니다.
-			# [{'GEN.1.1': '처음에 하나님이 하늘과 땅을 창조하셨다.}, {'GEN.1.2': '땅은 거칠고 비어 있었다. 어둠이 깊은 물 위에 깔려... ...]
+			# verse_maps는 구절 본문을 저장하며
+			# id: 서명.장.절
+			# text: 그 절의 내용
+			# 으로 하는 딕셔너리의 리스트입니다.
+			# [ ... {'id': 'GEN.5.32', 'text': ' 노아는 500세가 되어 셈, 함, 야벳을 낳았다.'} ... ]
+			# 인용되어 개행한 후 들여쓰기 된 구절은 \n을 통해 표현하였습니다.
 			all_verse_maps.extend(ch_verse_maps)
 
 			# 제목 전처리
@@ -212,25 +209,16 @@ def main():
 
 				source = child.get_attribute("data-verse-id")
 				text = title.get_attribute("textContent")
-				ch_title_maps.append({source: text})
-			# title_maps는 제목을 저장하며, 자신 바로 뒤에 나오는 구절의 서명.장.절을 키로 하고 자신의 내용을 값으로 하는 딕셔너리의 리스트입니다.
-			# [{'GEN.1.1': '하나님이 온 누리를 지으시다'}, ...]
+				title_type = title.get_attribute("class")
+				ch_title_maps.append({'id': source, 'text': text, 'type':title_type})
+			
+			# title_maps는 제목을 저장하며
+			# id: 자신 바로 뒤에 나오는 구절의 서명.장.절
+			# text: 제목의 내용
+			# type: 제목의 유형(ms: 대제목, s: 소제목)
+			# 으로 하는 딕셔너리의 리스트입니다.
+			# [{'id': 'GEN.1.1', 'text': '하나님이 온 누리를 지으시다', 'type': 's'}, ...
 			all_title_maps.extend(ch_title_maps)
-
-			# 소제목 전처리
-			ch_subtitle_maps = []
-			for subtitle in ch_subtitles:
-				# 바로 뒤에 오는 형제 요소의 자식 중 verse-span인 요소를 찾습니다.
-				next_sibling = subtitle.find_element(By.XPATH, "following-sibling::*[.//*[contains(@class,'verse-span')]][1]")
-				child = next_sibling.find_element(By.CLASS_NAME, "verse-span")
-
-				source = child.get_attribute("data-verse-id")
-				text = subtitle.get_attribute("textContent")
-				ch_subtitle_maps.append({source: text})
-
-			# subtitle_maps는 소제목을 저장하며, 자신 바로 뒤에 나오는 구절의 서명.장.절을 키로 하고 자신의 내용을 값으로 하는 딕셔너리의 리스트입니다.
-			# [{'GEN.1.1': '하나님이 온 누리를 지으시다'}, ...]
-			all_subtitle_maps.extend(ch_subtitle_maps)
 
 			# 단락 전처리
 			ch_paragraph_maps = []
@@ -238,15 +226,17 @@ def main():
 				child = paragraph.find_element(By.CLASS_NAME, "verse-span")
 
 				source = child.get_attribute("data-verse-id")
-				text = paragraph.get_attribute("class")
-				ch_paragraph_maps.append({source: text})
+				paragraph_type = paragraph.get_attribute("class")
+				ch_paragraph_maps.append({'id': source, 'type':paragraph_type})
 			
-			# paragraph_maps는 구절들의 집합인 단락을 저장하며, 단락 안에서 처음 나오는 구절의 서명.장.절을 키로 하고 단락의 종류를 값으로 하는 딕셔너리의 리스트입니다.
-			# 단락의 종류는 p, m이 있습니다. p는 평범한 본문들의 집합입니다. m은 q1 이후 나타나는 단락으로, p와 차이점은 없어 보입니다.
-			# [{'GEN.1.1': 'p'}, {'GEN.1.6': 'p'}, {'GEN.1.9': 'p'}, ...]
+			# paragraph_maps는 구절들의 집합인 단락을 저장하며, 
+			# id: 단락 안에서 처음 나오는 구절의 서명.장.절
+			# type: 단락의 유형
+			# 	단락의 종류는 p, m이 있습니다. p는 평범한 본문들의 집합입니다. m은 q1 이후 나타나는 단락으로, p와 차이점은 없어 보입니다.
+			# 으로 하는 딕셔너리의 리스트입니다.
+			# [{'id': 'GEN.1.1', 'type': 'p'}, {'id': 'GEN.1.6', 'type': 'p'}, {'id': 'GEN.1.9', 'type': 'p'} ...
+			
 			all_paragraph_maps.extend(ch_paragraph_maps)
-
-			# 인용 전처리
 
 			# 각주 전처리
 			ch_footnote_maps = []
@@ -268,16 +258,18 @@ def main():
 
 				source = verse_source + "." + str(char_source)
 				text = footnote.get_attribute("textContent")
-				ch_footnote_maps.append({source: text})
+				ch_footnote_maps.append({'id': source, 'text': text})
 			
-			# footname_maps는 각주를 저장하며, 구절의 서명.장.절.{자신이 나오기 전 해당 절의 글자 수}를 키로 하고 자신의 내용을 값으로 하는 딕셔너리의 리스트입니다.
-			# GEN.1.2.39 각주는 창세기 1장 2절의 39글자(띄어쓰기 포함) 이후 바로 각주가 나타난다는 뜻입니다.
-			# [{'GEN.1.1.0': '또는 ‘태초에’'}, {'GEN.1.2.39': '또는 ‘하나님의 바람’'}, {'GEN.1.21.7': '또는 ‘바다짐승들’'}, ...]
+			# footname_maps는 각주를 저장하며
+			# id: 구절의 서명.장.절.{자신이 나오기 전 해당 절의 글자 수}
+			# text: 각주의 내용
+			# GEN.1.2.39 각주는 창세기 1장 2절의 39글자(띄어쓰기 포함) 이후 각주가 나타난다는 뜻입니다.
+			# {'id': 'GEN.1.1.0', 'text': '또는 ‘태초에’'}, {'id': 'GEN.1.2.39', 'text': '또는 ‘하나님의 바람’'} ...
 			all_footnote_maps.extend(ch_footnote_maps)
 
 		
 		print(all_verse_maps)
-		print(all_subtitle_maps)
+		print(all_title_maps)
 		print(all_paragraph_maps)
 		print(all_footnote_maps)
 
